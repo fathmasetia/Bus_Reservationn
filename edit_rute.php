7215d6910c5e7de62bf96dd208569d1fc5535505
Jesica Purba A. <jesica.purba30@smk.belajar.id>
2025-03-05 13:14:19 +0700

    update

--- edit_rute.php
+++ edit_rute.php
@@ -1,1 +1,52 @@
+<?php
+include 'config.php';
 
+include'.includes/header.php';
+
+$postIdToEdit = $_GET['rute_id'];
+
+$query = "SELECT * FROM rute WHERE rute_id = $postIdToEdit";
+$result = $conn->query($query);
+
+if ($result->num_rows > 0) {
+    $post = $result->fetch_assoc();
+
+} else {
+    echo "Post not found.";
+    exit();
+}
+?>
+
+<div class="container-xxl flex-grow-1 container-p-y">
+ <div class="row">
+  <div class="col-md-10">
+   <div class="card mb-4">
+    <div class="card-body">
+     <form method="RUTE" action="proses_rute.php" enctype="multipart/form-data">
+      <input type="hidden" name="rute_id" value="<?php echo $postIdToEdit; ?>">
+
+      <div class="mb-3">
+     <label for="post_title" class="form-label">Kota Asal</label>
+    <input type="text" class="form-control" id="kota_asal" name="kota_asal" value="<?php echo $post['kota_asal']; ?>" required>
+  </div>
+
+<div class="mb-3">
+ <label for="category_id" class="form-label">Kota Tujuan</label>
+  <input type="text" class="form-control" id="kota_tujuan" name="kota_tujuan" required>
+ 
+        
+       
+    <div class="mb-3">
+  <label for="content" class="form-label">Harga</label>
+<selected_val class="form-control" id="harga" name="harga" required><?php echo $post['harga']; ?>
+ </div>
+  <button type="submit" name="update" class="btn btn-primary">Update</button>
+    </form>
+        </div>
+            </div>
+                </div>
+                     </div>
+                         </div>
+<?php
+include '.includes/footer.php';
+?>
--- proses_pesanan.php
+++ proses_pesanan.php
@@ -1,12 +1,64 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1>Hallo</h1>
-    <p>oh</p>
-</body>
-</html>
+<?php
+
+include("config.php");
+session_start();
+if (isset($_POST['simpan'])) {
+    $tanggal_pemesanan = $_POST['tanggal_pemesanan'];
+    $query = "INSERT INTO pemesanan VALUES('$tanggal_pemesanan')";
+    $exec = mysqli_query($conn, $query);
+
+    if ($exec) {
+        $_SESSION['notification'] = [
+            'type' => 'primary',
+            'message' => 'Pesanan berhasil ditambahkan!'
+        ];
+    } else {
+        $_SESSION['notification'] = [
+            'type' => 'danger',
+            'message' => 'Gagal menambahkan pesanan: ' . mysqli_error($conn)
+        ];
+    }
+
+    header('Location: pesanan.php');
+    exit();
+}
+
+if (isset($_POST['delete'])) {
+    $catID = $_POST['catID'];
+
+    $exec = mysqli_query($conn, "DELETE FROM pemesanan WHERE pemesanan_id='$catID'");
+    if ($exec) {
+        $_SESSION['notification'] = [
+            'type' => 'primary',
+            'message' => 'Pesanan berhasil dihapus!'
+        ];
+    } else {
+        $_SESSION['notification'] = [
+            'type' => 'danger',
+            'message' => 'Gagal menghapus pesanan: ' . mysqli_error($conn)
+        ];
+    }
+    header('Location: pesanan.php');
+    exit();
+}
+
+if (isset($_POST['update'])) {
+    $catID = $_POST['catID'];
+    $tanggal_pemesanan = $_POST['tanggal_pemesanan'];
+    $query = "UPDATE pemesanan SET tanggal_pemesanan = '$nama' WHERE pemesanan_id='$catID'";
+    $exec = mysqli_query($conn, $query);
+
+    if ($exec) {
+        $_SESSION['notification'] = [
+            'type' => 'primary',
+            'message' => 'Pesanan berhasil diperbarui!'
+        ];
+    } else {
+        $_SESSION['notification'] = [
+            'type' => 'danger',
+            'message' => 'Pesanan memperbarui kategori: ' . mysqli_error($conn)
+        ];
+    }
+    header('Location: pesanan.php');
+    exit();
+}
