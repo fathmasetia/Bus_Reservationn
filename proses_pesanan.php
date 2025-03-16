b148ce052ff884761397b219ba3663449ebce215
thalia-sarah <ritasaparina1983@gmail.com>
2025-03-16 22:33:53 +0700

    bugfix setelah konsul

--- proses_pesanan.php
+++ proses_pesanan.php
@@ -7,7 +7,6 @@ if (isset($_POST['simpan'])) {
     $rute_id = $_POST["rute_id"];
     $tanggalPesan = $_POST['tanggal_pemesanan'];
     $query = "INSERT INTO pemesanan (rute_id, penumpang_id, tanggal_pemesanan) VALUES ('$rute_id', '$penumpangId', '$tanggalPesan')";
-    $exec = mysqli_query($conn, $query);
 
     if ($conn->query($query) === TRUE){
         $_SESSION['notification'] = [
@@ -45,10 +44,9 @@ if (isset($_POST['delete'])) {
 
 if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['update'])){
     $pemesananId = $_POST['pemesanan_id'];
-    $penumpangId = $_POST['penumpang_id'];
     $rute_id = $_POST["rute_id"];
     $tanggalPesan = $_POST['tanggal_pemesanan'];
-    $query = "UPDATE pemesanan SET penumpang_id ='$penumpangId', rute_id ='$rute_id', tanggal_pemesanan = '$tanggalPesan' WHERE pemesanan_id = $pemesananId";
+    $query = "UPDATE pemesanan SET  rute_id ='$rute_id', tanggal_pemesanan = '$tanggalPesan' WHERE pemesanan_id = $pemesananId";
     $exec = mysqli_query($conn, $query);
 
     if ($conn->query($query)===TRUE){
